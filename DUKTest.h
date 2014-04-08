0d1dab30230cca71aaa8139c2049d22c67ab5f13
Matthias Drews <matthiasdrews@me.com>
2014-04-08 09:32:12 +0200

    add Test

---
+++ DUKTest.h
@@ -0,0 +1,13 @@
+//
+//  DUKTest.h
+//  Git Test
+//
+//  Created by Matthias Drews on 08.04.14.
+//  Copyright (c) 2014 drewsundkerner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DUKTest : NSObject
+
+@end
